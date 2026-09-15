da6020a431e6e3030dbcd2061de90a70acc480d7
Ryan Berube <berubr@rpi.edu>
2026-09-15 13:48:49 -0400

    Working gpio flash, thought default tri state was output, was wrong

--- test_files/gpio_test/gpio_toggle.c
+++ test_files/gpio_test/gpio_toggle.c
@@ -2,16 +2,18 @@
 
 int main(void) {
 
-  // pins should be output by default, no need for any config step
-
   // define the gpio register
   volatile uint32_t *gpio = (volatile uint32_t *)0x20003000;
+
+  volatile uint32_t *gpio_t_state = gpio + 1;
+
+  *gpio_t_state = 0x00000000;
 
   uint32_t toggle_helper = 0;
 
   volatile int i = 0;
   while (1) {
-    if (i == 100000000) {
+    if (i == 2000000) {
       // toggle GPIO
 
       toggle_helper = ~toggle_helper;
